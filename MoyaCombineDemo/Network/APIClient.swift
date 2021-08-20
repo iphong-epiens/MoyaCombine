24f59c010f3c8326b65e483979ea65eec3356db4
Inpyo Hong <y2kpaulh@epiens.com>
2021-08-20 15:39:24 +0900

    refresh token error, access token error, public key error 예외 조건 추가

--- MoyaCombineDemo/Network/APIClient.swift
+++ MoyaCombineDemo/Network/APIClient.swift
@@ -77,61 +77,86 @@ final public class API: ObservableObject {
     }
     /// Default api client
     public static let shared: NetworkClient = {
-
-      let networkClosure = {(_ change: NetworkActivityChangeType, _ target: TargetType) in
-        DispatchQueue.main.async {
-          switch change {
-          case .began: break
-            //HUD.show(.progress, onView: AppDelegate.root.view)
-          case .ended:
-            HUD.hide(afterDelay: 1.0)
-          }
+        
+        let networkClosure = {(_ change: NetworkActivityChangeType, _ target: TargetType) in
+            DispatchQueue.main.async {
+                switch change {
+                case .began: break
+                //HUD.show(.progress, onView: AppDelegate.root.view)
+                case .ended:
+                    HUD.hide(afterDelay: 1.0)
+                }
+            }
         }
-      }
-
-      var configuration: URLSessionConfiguration {
-        let config = URLSessionConfiguration.default
-        config.timeoutIntervalForRequest = 5
-        config.timeoutIntervalForResource = 5
-        config.requestCachePolicy = .useProtocolCachePolicy
-        return config
-      }
-
-      let sessionConfig = Session(configuration: configuration, startRequestsImmediately: false)
-      let logOptions: NetworkLoggerPlugin.Configuration = NetworkLoggerPlugin.Configuration(logOptions: .verbose)
-
-      let plugIn: [PluginType] = [NetworkLoggerPlugin(configuration: logOptions), NetworkActivityPlugin(networkActivityClosure: networkClosure), AccessTokenPlugin(tokenClosure: { _ in
-        var accessToken: String {
-//          guard let token = try? KeyChain.getString("accessToken") else { return "" }
-//          print("saved Token", token)
-            return "" //token
+        
+        var configuration: URLSessionConfiguration {
+            let config = URLSessionConfiguration.default
+            config.timeoutIntervalForRequest = 5
+            config.timeoutIntervalForResource = 5
+            config.requestCachePolicy = .useProtocolCachePolicy
+            return config
         }
-
-        return accessToken
-      })]
-
-      let provider = MoyaProvider<MultiTarget>(plugins: plugIn)
-      let client = NetworkClient(provider: provider)
-
-      return client
+        
+        let sessionConfig = Session(configuration: configuration, startRequestsImmediately: false)
+        let logOptions: NetworkLoggerPlugin.Configuration = NetworkLoggerPlugin.Configuration(logOptions: .verbose)
+        
+        let plugIn: [PluginType] = [NetworkLoggerPlugin(configuration: logOptions), NetworkActivityPlugin(networkActivityClosure: networkClosure), AccessTokenPlugin(tokenClosure: { _ in
+            var accessToken: String {
+                //          guard let token = try? KeyChain.getString("accessToken") else { return "" }
+                //          print("saved Token", token)
+                return "" //token
+            }
+            
+            return accessToken
+        })]
+        
+        let provider = MoyaProvider<MultiTarget>(plugins: plugIn)
+        let client = NetworkClient(provider: provider)
+        
+        return client
     }()
-
+    
     // API singleton
     private init() {}
 }
 
 extension API.NetworkClient {
     func request<Request: TargetType>(_ request: Request) -> AnyPublisher<Moya.Response, Error> {
-      let target = MultiTarget(request)
+        let target = MultiTarget(request)
 
         return self.provider.requestPublisher(target)
             .tryMap{
                 if self.hasValidRefreshToken {
-                    throw SwsApiError.refreshTokenError
+                  throw SwsApiError.refreshTokenError
                 }
-                else if let statusCode = $0.response?.statusCode, statusCode == 200 {
-                    throw SwsApiError.refreshTokenError
+                
+                let json = try $0.mapJSON()
+                
+                if let object = json as? [String: Any], let resultData = object["jsonData"] as? [String: Any], let statusCode = resultData["code"] as? Int, let resultCode = resultData["resultCode"] as? String {
+                    
+                    let status = HTTPStatusCode(rawValue: statusCode)
+                    let result = ResultCode(rawValue: resultCode)
+                    
+                    print((#function, statusCode))
+                    
+                    switch status {
+                    case .ok:
+                        switch result {
+                        case .authError:
+                            throw SwsApiError.accessTokenError
+
+                        case .publicKeyError:
+                            throw SwsApiError.publicKeyError
+
+                        default:
+                            break
+                        }
+                        
+                    default:
+                        break
+                    }
                 }
+                
                 return $0
             }
             .receive(on: DispatchQueue.global(qos: .background))
@@ -139,7 +164,19 @@ extension API.NetworkClient {
                 switch completion {
                 case .failure(let error as SwsApiError):
                     print(">>> SwsApiError", error)
-                    
+                    switch error {
+                    case .refreshTokenError:
+                        print("refreshTokenError")
+                        
+                    case .accessTokenError:
+                        print("accessTokenError")
+                        
+                    case .publicKeyError:
+                        print("publicKeyError")
+
+                    default:
+                        break
+                    }
                 default:
                     break
                 }
@@ -149,45 +186,45 @@ extension API.NetworkClient {
     }
     
     func getPublicKey() {
-
+        
         _ = self.request(ReqAPI.Auth.publickey())
             .sink(receiveCompletion: {
                 print($0)
             }, receiveValue: {
                 print($0)
             })
-         
-       
         
-//      return API.shared.request(ReqAPI.Auth.publickey())
-//        .print()
-//        .receive(on: DispatchQueue.global(qos: .background))
-////        .handleEvents(receiveOutput: { ouput in
-////            print(ouput)
-////        }, receiveCompletion: { completion in
-////            print(completion)
-////        })
-//        .eraseToAnyPublisher()
-//        .subscribe(onSuccess: { response in
-//          do {
-//            let json = try response.mapJSON()
-//            if let object = json as? [String: Any],
-//               let resultData = object["jsonData"],
-//               let jsonData = resultData as? [String: Any],
-//               let res = jsonData["res"]  as? [String: Any],
-//               let publicKey = res["publicKey"] as? String {
-//              //save public key
-//              try KeyChain.set(publicKey, key: "publicKey")
-//              print("new publicKey:", publicKey)
-//              single(.success(response))
-//            }
-//          } catch let error {
-//            print(error.localizedDescription)
-//            single(.error(error))
-//          }
-//        }) { error in
-//          single(.error(error))
-//        }
-
+        
+        
+        //      return API.shared.request(ReqAPI.Auth.publickey())
+        //        .print()
+        //        .receive(on: DispatchQueue.global(qos: .background))
+        ////        .handleEvents(receiveOutput: { ouput in
+        ////            print(ouput)
+        ////        }, receiveCompletion: { completion in
+        ////            print(completion)
+        ////        })
+        //        .eraseToAnyPublisher()
+        //        .subscribe(onSuccess: { response in
+        //          do {
+        //            let json = try response.mapJSON()
+        //            if let object = json as? [String: Any],
+        //               let resultData = object["jsonData"],
+        //               let jsonData = resultData as? [String: Any],
+        //               let res = jsonData["res"]  as? [String: Any],
+        //               let publicKey = res["publicKey"] as? String {
+        //              //save public key
+        //              try KeyChain.set(publicKey, key: "publicKey")
+        //              print("new publicKey:", publicKey)
+        //              single(.success(response))
+        //            }
+        //          } catch let error {
+        //            print(error.localizedDescription)
+        //            single(.error(error))
+        //          }
+        //        }) { error in
+        //          single(.error(error))
+        //        }
+        
     }
 }
