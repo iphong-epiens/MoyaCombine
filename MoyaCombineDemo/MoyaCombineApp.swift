fbf133d88718b8a164df2404524160599fbe3a21
Inpyo Hong <y2kpaulh@epiens.com>
2021-08-20 14:20:59 +0900

    custom url scheme 루틴 추가

--- MoyaCombineDemo/MoyaCombineApp.swift
+++ MoyaCombineDemo/MoyaCombineApp.swift
@@ -17,41 +17,49 @@ struct MoyaCombineApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @Environment(\.scenePhase) private var scenePhase
     @StateObject var settings: AppSettings = AppSettings()
-  //  static let keychain = Keychain(service: Bundle.main.bundleIdentifier!).accessibility(.afterFirstUnlock)
-
+    //  static let keychain = Keychain(service: Bundle.main.bundleIdentifier!).accessibility(.afterFirstUnlock)
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(settings)
                 .onAppear{
-//                    print(FileManager.documentURL ?? "")
-//                    for fontFamily in UIFont.familyNames {
-//                        for fontName in UIFont.fontNames(forFamilyName: fontFamily) {
-//                            print(fontName)
-//                        }
-//                    }
+                    //                    print(FileManager.documentURL ?? "")
+                    //                    for fontFamily in UIFont.familyNames {
+                    //                        for fontName in UIFont.fontNames(forFamilyName: fontFamily) {
+                    //                            print(fontName)
+                    //                        }
+                    //                    }
                 }
-                .onOpenURL { _ in // URL handling
-
+                .onOpenURL { url in
+                    // call my-deeplink-test://share?id=1234
+                    if (url.scheme == "my-deeplink-test" && url.host == "share") {
+                        if let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true) {
+                            for query in components.queryItems! {
+                                print(query.name)
+                                print(query.value!)
+                            }
+                        }
+                    }
                 }
                 .onChange(of: scenePhase) { phase in
-                  // change in this app's phase - composite of all scenes
-                  switch phase {
-                  case .active:
-                    //changedToActive()
-                    print("active")
-
-                  case .background:
-                    //changedToBackground()
-                    print("background")
-
-                  case .inactive:
-                    //changedToInactive()
-                    print("inactive")
-
-                  default:
-                    break
-                  }
+                    // change in this app's phase - composite of all scenes
+                    switch phase {
+                    case .active:
+                        //changedToActive()
+                        print("active")
+                        
+                    case .background:
+                        //changedToBackground()
+                        print("background")
+                        
+                    case .inactive:
+                        //changedToInactive()
+                        print("inactive")
+                        
+                    default:
+                        break
+                    }
                 }
         }
     }
